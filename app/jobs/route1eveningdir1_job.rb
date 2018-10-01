643ebc0bb6007841366341a25807025186e2bf4e
zoey <zoey@zoeydeMacBook-Pro.local>
2018-10-01 00:01:11 -0300

    fix

--- app/jobs/route1eveningdir1_job.rb
+++ app/jobs/route1eveningdir1_job.rb
@@ -4,12 +4,11 @@ class Route1eveningdir1Job
     ms = ['Mon','Tue','Wed',"Thu",'Fri','Sat']
       if Time.now.strftime('%H').to_i > 18 && Time.now.strftime('%H').to_i < 24 &&  ms.include?(Date.today.strftime("%a"))
         Route1eveningdir1Worker.perform_async('Route1eveningdir1Job')
-      else
-        1.upto(5) do |n|
+      end
+        1.upto(7) do |n|
           puts n
           sleep 1
         end
-      end
   end
 
 
