40dd7be753d37a59a3bc6fd5f648995b2f1f5b0b
zoey <zoey@zoeydeMacBook-Pro.local>
2018-10-03 19:30:12 -0300

    fix

--- app/jobs/route1eveningdir1_job.rb
+++ app/jobs/route1eveningdir1_job.rb
@@ -2,12 +2,12 @@ class Route1eveningdir1Job
 
   def perform
     ms = ['Mon','Tue','Wed',"Thu",'Fri','Sat']
-      if Time.now.strftime('%H').to_i > 18 && Time.now.strftime('%H').to_i < 24 &&  ms.include?(Date.today.strftime("%a"))
+      if Time.now.strftime('%H').to_i > 17 && Time.now.strftime('%H').to_i < 24 &&  ms.include?(Date.today.strftime("%a"))
         Route1eveningdir1Worker.perform_async('Route1eveningdir1Job')
       end
         1.upto(7) do |n|
           puts n
-          sleep 1
+          # sleep 1
         end
   end
 
--- app/jobs/route1eveningdir2_job.rb
+++ app/jobs/route1eveningdir2_job.rb
@@ -2,12 +2,12 @@ class Route1eveningdir2Job
 
   def perform
         ms = ['Mon','Tue','Wed',"Thu",'Fri','Sat']
-      if Time.now.strftime('%H').to_i > 18 && Time.now.strftime('%H').to_i < 24 && ms.include?(Date.today.strftime("%a"))
+      if Time.now.strftime('%H').to_i > 17 && Time.now.strftime('%H').to_i < 24 && ms.include?(Date.today.strftime("%a"))
         Route1eveningdir2Worker.perform_async('Route1eveningdir2Job')
       end
         1.upto(7) do |n|
           puts n
-          sleep 1
+          # sleep 1
         end
   end
 
